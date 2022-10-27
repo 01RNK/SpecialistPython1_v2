0db6ff354af814b6aed0ecd1f7adb8694405a622
RNK <116557763+01RNK@users.noreply.github.com>
2022-10-27 20:04:58 +0300

    Update 02_hw_func.py
    Не до конца понял как из сформированного числового массива выбрать наименьший элемент и вывести его имя. Вероятно ошибка концептуальная -- в подходе

--- Module6/home_work/02_hw_func.py
+++ Module6/home_work/02_hw_func.py
@@ -9,5 +9,39 @@ def distance(x1, y1, x2, y2):
     pass
 
 
-# TODO: your code here
-print("Самый короткий отрезок:", ...)  # Выводим название отрезка, например “АС”.
+def distance(x1, x2, y1, y2):
+    dist_point = float(((x1 - x2) ** 2 + (y1 - y2) ** 2) * 0.5)
+    return dist_point
+
+
+x1 = float(input("x1 = "))
+y1 = float(input("y1 = "))
+x2 = float(input("x2 = "))
+y2 = float(input("y2 = "))
+
+AB = distance(x1, x2, y1, y2)
+print("AB = ", AB)
+
+x1 = float(input("x1 = "))
+y1 = float(input("y1 = "))
+x2 = float(input("x2 = "))
+y2 = float(input("y2 = "))
+
+BC = distance(x1, x2, y1, y2)
+print("BC = ", BC)
+
+x1 = float(input("x1 = "))
+y1 = float(input("y1 = "))
+x2 = float(input("x2 = "))
+y2 = float(input("y2 = "))
+
+CA = distance(x1, x2, y1, y2)
+print("CA = ", CA)
+
+nums = [AB, BC, CA]
+if nums[0]==AB:
+    print("Самый короткий отрезок: AB")
+elif nums[0] == BC:
+    print("Самый короткий отрезок: BC")
+else:
+    print("Самый короткий отрезок: CA")
